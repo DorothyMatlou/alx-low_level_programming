7f7e25375271e963946a6374716c81a08a61702d
DorothyMatlou <dorothymaleka@gmail.com>
2023-04-20 06:02:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that introduces the puts function
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
