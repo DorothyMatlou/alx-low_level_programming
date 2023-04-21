ea402fa9bffae51f386ddb7d5134dfba97a0457a
DorothyMatlou <dorothymaleka@gmail.com>
2023-04-21 07:55:42 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - program that prints all numbers of base 16
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int d;
+	char alp;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d)
+			for (alp = 'a'; alp <= 'f'; alp++)
+				putchar(alp);
+	putchar('\n');
+	return (0);
+}
