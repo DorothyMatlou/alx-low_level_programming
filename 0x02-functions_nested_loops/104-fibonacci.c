f535dd61ed357e36c87582351d8fcb26788b8e57
DorothyMatlou <dorothymaleka@gmail.com>
2023-04-26 12:54:53 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - finds and prints the first 98 Fibonacci numbers
- * Starting with 1 and 2 followed by a new line
- *Return: Always (Success)
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return:  (Success)
  */
+
 int main(void)
 {
 	unsigned long int i;
@@ -16,17 +20,20 @@ int main(void)
 	unsigned long int aft2;
 
 	printf("%lu", bef);
-	for (i = 1, i < 91, i++)
+
+	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", aft);
 		aft += bef;
 		bef = aft - bef;
 	}
+
 	bef1 = (bef / l);
 	bef2 = (bef % l);
 	aft1 = (aft / l);
 	aft2 = (aft % l);
-	for (i = 92, i < 99; ++i)
+
+	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", aft1 + (aft2 / l));
 		printf("%lu", aft2 % l);
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,32 +1,35 @@
 #include "main.h"
 /**
  * time_table - prints the 9 times table from zero
+ * Return: empty output
  */
 void time_table(void)
 {
-	int i, j, k;
+	int i, j, k, l, m;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			k = j * i;
-			if (j ==0)
+			k = i * j;
+			if (k > 9)
 			{
-				_putchar(k + '0');
+				l = k % 10;
+				m = (k - l) / 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(m + '0');
+				_putchar(l + '0');
 			}
-			if (k < 10 && j !=0)
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar(k + '0');
-			}else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
