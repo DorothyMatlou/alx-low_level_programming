1738fe1211d9e82bb789f6db1a5cc2832dcbd8e6
DorothyMatlou <dorothymaleka@gmail.com>
2023-05-18 07:19:52 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -21,7 +21,7 @@ int is_digit(char *s)
 }
 
 /**
- * _strlen - A function thatreturns the length of a string
+ * _strlen - A function that returns the length of a string
  * @s: input string
  * Return: the length of the string
  */
@@ -57,24 +57,28 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-errors();
-len1 = _strlen(s1);
-len2 = _strlen(s2);
-len = len1 + len2 + 1;
-result = malloc(sizeof(int) * len);
-if (!result)
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+	errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
 	return (1);
-for (i = 0; i <= len1 + len2; i++)
+	for (i = 0; i <= len1 + len2; i++)
 	result[i] = 0;
-for (len1 = len1 - 1; len1 >= 0; len1--)
-{
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
 	digit1 = s1[len1] - '0';
 	carry = 0;
 	for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 	{
 		digit2 = s2[len2] - '0';
 		carry += result[len1 + len2 + 1] + (digit1 * digit2);
+		result[len1 + len2 + 1] = carry % 10;
+		carry /= 10;
+	}
+	if (carry > 0)
 		result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
@@ -88,6 +92,5 @@ for (len1 = len1 - 1; len1 >= 0; len1--)
 		_putchar('0');
 	_putchar('\n');
 	free(result);
-}
 	return (0);
 }
